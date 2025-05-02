65f12118480d12f8c15d2d849ef4fc9523a10b43
Atul Raj <159701250+AtulRaj1@users.noreply.github.com>
2025-05-02 12:31:33 +0530

    Add files via upload

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero
+          voluptates odio molestias voluptatibus consequuntur quis cumque, quam
+          laborum distinctio modi ex labore officiis earum qui ducimus at dolore
+          in consectetur, veniam quia? Hic, quod. Explicabo hic unde delectus
+          eveniet obcaecati.
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
